2319cf9bfae06bf85f4ea197f8aa6a87643e315e
martin.majer <mmajer@students.zcu.cz>
2015-02-18 21:19:04 +0100

    resize

--- notebooks/week02ukoly.py
+++ notebooks/week02ukoly.py
@@ -52,6 +52,25 @@ def list_filepaths(root):
 
 # <codecell>
 
+def keep_ratio(shape):
+    height = 227.
+    width = 227.
+    
+    if shape[0] <= shape[1]: #height <= width
+        ratio = height / shape[0]
+        print ratio
+        width = shape[1] * ratio
+    else:
+        ratio = width / shape[1]
+        print ratio
+        height = shape[0] * ratio
+    
+    dim = int(width), int(height) #opencv dimension format
+    
+    return dim
+
+# <codecell>
+
 root = '../data/SUN2012/Images/'
 
 imgs_path = list_filepaths(root)
@@ -60,4 +79,44 @@ sample = sorted(imgs_path)  #copy
 np.random.seed(50)
 np.random.shuffle(sample) #in-place
 sample = sample[:10000]
+
+# <codecell>
+
+filename = 'sun_sample.hdf5'
+
+with h5py.File(filename,'w') as f:
+
+# <codecell>
+
+img1 = cv2.imread('../data/1.jpg')
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+img2 = cv2.imread('../data/2.jpg')
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img3 = cv2.imread('../data/2.jpg')
+img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+img4 = cv2.imread('../data/4.png')
+img4 = cv2.cvtColor(img4, cv2.COLOR_BGR2RGB)
+
+dim = keep_ratio(img1.shape)
+img_resized = cv2.resize(img1, dim, interpolation = cv2.INTER_CUBIC)
+
+plt.figure()
+plt.imshow(img_resized)
+plt.show()
+
+
+
+
+
+
+ok = [img1, img2, img3]
+
+for img in ok:
+    if len(img.shape) == 3:
+        print 'ok'
+
+
+
+#for img_path in sample[:2]:
+    
 
