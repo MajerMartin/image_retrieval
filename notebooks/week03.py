7cd1c3da4ab91c1720b1958eb6df82ef0e7fd0bb
martin.majer <mmajer@students.zcu.cz>
2015-02-26 22:27:49 +0100

    custom distance matrix and k nearest neighbors - square matrix using transposition

--- notebooks/week03.py
+++ notebooks/week03.py
@@ -6,6 +6,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
+from scipy.spatial.distance import pdist, squareform
 
 %matplotlib inline
 
@@ -17,21 +18,47 @@ n = 10000
 
 # <codecell>
 
-with h5py.File(h5_imgs_fn,'r') as fr_imgs, h5py.File(h5_fts_fn,'r') as fr_features:
-    features = np.copy(fr_features['score'])
-    print features.shape
-    dst_matrix = get_distance_matrix(features, n)
-    print dst_matrix.shape
+with h5py.File(h5_fts_fn,'r') as fr_features:
+    features_score = np.copy(fr_features['score'])
+    print 'features_score: ', features_score.shape
+    features_fc7 = np.copy(fr_features['blob_fc7'][:,4,:])
+    print 'features_fc7: ', features_fc7.shape
+
+# <codecell>
+
+dst_m_score = get_distance_matrix(features_score, n)
+print '\nscore: ', dst_m_score.shape
+
+dst_m_fc7 = get_distance_matrix(features_fc7, n)
+print '\nfc7: ', dst_m_fc7.shape
+
+dst_m_score_sp = pdist(features_score, 'euclidean')
+print 'score_scipy: ', dst_m_score_sp.shape
+
+dst_m_fc7_sp = pdist(features_fc7, 'euclidean')
+print 'fc7_scipy: ', dst_m_fc7_sp.shape
+
+# <codecell>
+
+dst_m_score_squareform = squareform(dst_m_score_sp)
+print 'score_scipy_squareform: ', dst_m_score_squareform.shape
+
+dst_m_fc7_squareform = squareform(dst_m_fc7_sp)
+print 'fc7_scipy_squareform: ', dst_m_fc7_squareform.shape
+
+# <codecell>
+
+print dst_m_score[:5,:5], '\n'
+print dst_m_score_squareform[:5,:5], '\n'
+print '-' * 35
+print dst_m_fc7[:5,:5], '\n'
+print dst_m_fc7_squareform[:5,:5]
 
 # <codecell>
 
 img_index = 1920
-k = 1
-find_k_nearest(dst_matrix, img_index, k)
-
-# <headingcell level=3>
-
-# Distance Matrix
+k = 3
+find_k_nearest(dst_m_score, img_index, k)
 
 # <codecell>
 
@@ -43,36 +70,25 @@ def get_distance_matrix(features, n):
         print '\r', 'i: ', i,
         for j in range(i, n):
             dst_matrix[i,j] = np.linalg.norm(features[i] - features[j])
-        
+    
+    dst_matrix += dst_matrix.T
+    
     return np.array(dst_matrix)
-
-# <headingcell level=3>
-
-# Distance Matrix - scipy
-
-# <headingcell level=3>
-
-# K-Nearest Neighbors
 
 # <codecell>
 
 def find_k_nearest(dst_matrix, img_index, k):
     
-    dst = dst_matrix[img_index,img_index:]
+    dst = dst_matrix[img_index,:]
     best_indexes = np.argsort(dst)
     
     with h5py.File(h5_imgs_fn,'r') as fr_imgs:
     
         for i in range(0, k+1):
-           index = best_indexes[i] + img_index
-           img = np.array(fr_imgs['imgs'][index][:,:,::-1])
+           img = np.array(fr_imgs['imgs'][best_indexes[i]][:,:,::-1])
            img = img.astype(np.float32) * 255
         
            plt.figure()
            plt.imshow(img.astype(np.uint8)[:,:,::-1])
            plt.show()
-
-# <headingcell level=3>
-
-# K-Nearest Neighbors - sklearn
 
