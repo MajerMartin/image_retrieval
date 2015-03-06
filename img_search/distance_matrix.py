cc33cb42a8ea685afd907b46731f05e1efd0caa6
martin.majer <mmajer@students.zcu.cz>
2015-03-06 15:49:04 +0100

    add rows and columns

--- img_search/distance_matrix.py
+++ img_search/distance_matrix.py
@@ -1,35 +1,88 @@
 __author__ = 'martin.majer'
 
+import numpy as np
+
 class ImageSearchDistanceMatrix(object):
-     def __init__(self, max_images=100000, thumbnail_size=(150,150,3)):
-         self.max_images = max_images
-         self.thumbnail_size = thumbnail_size
-         self.distance_matrix = None
-         self.images = []
-         self.features = []
+    def __init__(self, max_images=100000, thumbnail_size=(150,150,3)):
+        self.max_images = max_images
+        self.thumbnail_size = thumbnail_size
+        self.distance_matrix = None
+        self.images = []
+        self.features = []
 
-     def add_images(self, images, features):
-          # features - do teto metody vstupuji jiz zvolene typy priznaku
-          # images - zmensit dle self.thumbnail_size, fixni velikost
-          pass
+    def add_images(self, images, features):
+        # add features
+        if len(features.shape) == 1:
+            self.features.append(features)
+            start = len(self.features) - 1
+        else:
+            self.features.extend(features)
+            start = len(self.features) - len(features)
 
-     def find_k_nearest_by_index(self, img_index, k=3):
-          pass
+        end = len(self.features)
 
-     def get_images(self, indexes):
-         """
+        # initialize new distance matrix
+        if self.distance_matrix is None:
+            self.distance_matrix = np.zeros((end, end))
 
-         :param indexes:
-         :return:
-         """
-         images = []
-         for index in indexes:
-             images.append(self.images[index])
-         return images
+            # fill new distance matrix
+            for i in range(0, end):
+                for j in range(i, end):
+                    self.distance_matrix[i,j] = np.linalg.norm(self.features[i] - self.features[j])
 
-     def save(self, filename):
-          # ulozit do H5, klice images, features, max_images, thumbnail_size, distance_matrix (ulozit jen spodni trojuhelnik)
-          pass
+        # add rows and columns
+        else:
+            for i in range(start, end):
+                new_row = np.zeros((1, i))
+                self.distance_matrix = np.concatenate((self.distance_matrix, new_row), axis=0)
 
-     def load(self, filename):
-          pass
+                new_col = np.zeros((i + 1, 1))
+
+                for j in range(0, i):
+                    print "i:", i, ", j:", j
+                    new_col[j,0] = np.linalg.norm(self.features[i] - self.features[j])
+
+                self.distance_matrix = np.concatenate((self.distance_matrix, new_col), axis=1)
+
+
+
+
+
+
+        self.images.extend(images)
+
+
+        print "imgs: ", self.images
+        print "fts: ", len(self.features), '\n'
+
+
+
+        # zmensi obrazky
+        # prida nove obrazky do images
+        return self.distance_matrix
+        # features - do teto metody vstupuji jiz zvolene typy priznaku
+        # images - zmensit dle self.thumbnail_size, fixni velikost
+
+
+
+
+    def find_k_nearest_by_index(self, img_index, k=3):
+        pass
+
+    def get_images(self, indexes):
+        """
+
+        :param indexes:
+        :return:
+        """
+        images = []
+        for index in indexes:
+            images.append(self.images[index])
+        return images
+
+    def save(self, filename):
+        # ulozit do H5, klice images, features, max_images, thumbnail_size, distance_matrix (ulozit jen spodni trojuhelnik)
+        pass
+
+    def load(self, filename):
+        pass
--- img_search/tests/test_distance_matrix.py
+++ img_search/tests/test_distance_matrix.py
@@ -1,17 +1,41 @@
 import unittest
+import numpy as np
+import h5py
 
 import img_search.distance_matrix
 
+
+h5_imgs_fn = '/Users/martin.majer/PycharmProjects/PR4/data/sun_sample.hdf5'
+h5_fts_fn = h5_imgs_fn + '.features.hdf5'
+
+with h5py.File(h5_fts_fn,'r') as fr_features, h5py.File(h5_src_fn,'r') as fr_imgs:
+    features = np.copy(fr_features['score'])
+
+
 class DistanceMatrixTestClass(unittest.TestCase):
+
     def test_distance_matrix_init(self):
+
         dm = img_search.distance_matrix.ImageSearchDistanceMatrix()
 
-    def test_distance_matrix_get_images(self):
+        self.assertEqual(dm.max_images, 100000)
+        self.assertEqual(dm.thumbnail_size, (150,150,3))
+        self.assertEqual(dm.distance_matrix, None)
+        self.assertEqual(dm.images, [])
+        self.assertEqual(dm.features, [])
+
+    def test_distance_matrix_add_images(self):
+
         dm = img_search.distance_matrix.ImageSearchDistanceMatrix()
 
-        images = dm.get_images([])
+        dm.add_images([1,2,3], features[:3,:])
+        self.assertEqual(dm.distance_matrix.shape, (3,3))
 
-        self.assertEqual(images, [])
+        dm.add_images([4], features[3,:])
+        self.assertEqual(dm.distance_matrix.shape, (4,4))
+
+        dm.add_images([5,6], features[4:6,:])
+        self.assertEqual(dm.distance_matrix.shape, (6,6))
 
 if __name__ == '__main__':
     unittest.main()
