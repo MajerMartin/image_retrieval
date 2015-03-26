2011dba5a341c5b36a703e125dbc18a9e9b36050
martin.majer <mmajer@students.zcu.cz>
2015-03-26 21:34:30 +0100

    crop and keep_ratio functions

--- img_search/server/server_flask.py
+++ img_search/server/server_flask.py
@@ -46,6 +46,45 @@ else:
 
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
+def keep_ratio(shape, height, width):
+    '''
+    Calculates dimensions for resizing without affecting ratio.
+    :param shape: shape of array of image saved in numpy
+    :param height: height the image will be resized to
+    :param width: width the image will be resized to
+    :return: dimensions the image will be resized to
+    '''
+    #height <= width
+    if shape[0] <= shape[1]:
+        ratio = height / shape[0]
+        width = shape[1] * ratio
+    else:
+        ratio = width / shape[1]
+        height = shape[0] * ratio
+
+    #opencv dimension format
+    dim = int(width), int(height)
+
+    return dim
+
+def crop(img, height, width):
+    '''
+    Crop center of the image.
+    :param img: image to be cropped
+    :param height: height the image will be cropped to
+    :param width: width the image will be cropped to
+    :return: cropped image
+    '''
+    if (img.shape[0] == height) and (img.shape[1] == width):
+        return img
+    elif img.shape[0] == height:
+        middle = img.shape[1] / 2
+        return img[:, (middle - width / 2):(middle + width / 2)]
+    else:
+        middle = img.shape[0] / 2
+        return img[(middle - height / 2):(middle + height / 2), :]
+
+
 def check_allowed(filename):
     '''
     Check whether uploaded file is image or not.
