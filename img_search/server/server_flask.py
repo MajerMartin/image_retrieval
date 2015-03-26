1107e8444a8f2ab855d1a6111cf841087ce271c0
martin.majer <mmajer@students.zcu.cz>
2015-03-26 20:40:37 +0100

    caffe toggle

--- img_search/server/server_flask.py
+++ img_search/server/server_flask.py
@@ -3,41 +3,78 @@ from img_search import kdtree
 import os.path
 import cv2
 import urllib
-import caffe
+import numpy as np
 
-# local testing
-import h5py
-from random import randint
-
-# pak nebude potreba
-h5_imgs_fn = '/Users/martin.majer/PycharmProjects/PR4/data/sun_sample.hdf5'
-h5_fts_fn = h5_imgs_fn + '.features.hdf5'
-# jaky limit?
-n = 1000000
-
+# initialize flask server
 app = Flask(__name__)
 
-#  zmena na metacentrum
-app.config['UPLOAD_FOLDER'] = '/Users/martin.majer/PycharmProjects/PR4/data/kdt_server/'
+# switch between deployment (True) and local testing mode (False)
+caffe_toggle = False
+
+if caffe_toggle:
+    import caffe
+
+    # paths for caffe
+    caffe_root = '/storage/plzen1/home/campr/metacentrum/caffe/caffe_cemi_7_dev'
+    caffe_mean_fn = os.path.join(caffe_root, 'python/caffe/imagenet/ilsvrc_2012_mean.npy')
+    caffe_deploy_fn = os.path.join(caffe_root, 'models/bvlc_reference_caffenet/deploy.prototxt')
+    caffe_model_fn = os.path.join(caffe_root, 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+
+    # initialize caffe neural network
+    net = caffe.Classifier(caffe_deploy_fn,
+                       caffe_model_fn,
+                       mean=np.load(caffe_mean_fn),
+                       # imagenet uses BGR, same as opencv, swap is not needed:
+                       #channel_swap=(2,1,0),
+                       # bvlc_reference_caffenet uses pixel values [0, 255], but we don't normalize loaded image to [0.0-1.0], so it is scaled to [0.0, 255.0] already after conversion to float32
+                       #raw_scale=255,
+                       )
+    # run net on cpu
+    net.set_mode_cpu()
+
+    # path to data folder
+    app.config['UPLOAD_FOLDER'] = '/storage/plzen1/home/mmajer/pr4/data/image_search/'
+else:
+    import h5py
+    from random import randint
+
+    # path to h5df file with features
+    h5_fts_fn = '/Users/martin.majer/PycharmProjects/PR4/data/sun_sample.hdf5.features.hdf5'
+
+    # path to data folder
+    app.config['UPLOAD_FOLDER'] = '/Users/martin.majer/PycharmProjects/PR4/data/kdt_server/'
+
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
 def check_allowed(filename):
+    '''
+    Check whether uploaded file is image or not.
+    :param filename: name of uploaded image
+    :return: boolean value
+    '''
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 @app.route('/search')
 def search():
-    # uvodni stranka s vyhledavacim formularem
+    '''
+    Main page with form.
+    :return: rendered template from html file
+    '''
     return render_template('search.html')
 
 @app.route('/results', methods=['POST'])
 def results():
-    # vyhledavani, bud podle URL s obrazkem nebo podle nahraneho obrazku
+    '''
+    Add image to database, calculates features and finds nearest neighbors.
+    :return: rendered template from html file
+    '''
+
     search_file = request.files['search_file']
     search_url = request.form['search_url']
 
     if search_url:
-        msg = 'Searching using URL (%s)' % (search_url)
+        msg = 'Searching using URL (%s)' % search_url
         filenames = []
         print search_url
         filename = 'img.jpg'
@@ -45,7 +82,7 @@ def results():
         urllib.urlretrieve(search_url, path)
 
     elif search_file and check_allowed(search_file.filename):
-        msg = 'Searching using file (name %s)' % (search_file.filename)
+        msg = 'Searching using file (name %s)' % search_file.filename
 
         # docasne ulozit obrazek na disk
         filename = 'img.jpg'
