c7eee18053c614046858b4b3d45f2a91c564e261
martin.majer <mmajer@students.zcu.cz>
2015-03-28 11:40:17 +0100

    add __init__

--- img_search/server/server_flask.py
+++ img_search/server/server_flask.py
@@ -1,6 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+
 from flask import Flask, request, render_template, send_from_directory
 from img_search import kdtree
-import os.path
 import cv2
 import urllib
 import numpy as np
